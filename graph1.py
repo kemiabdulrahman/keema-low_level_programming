cafb92a27412cb34d8e03001200046338a806665
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-02-02 08:13:48 +0100

    task done

--- graph1.py
+++ graph1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, (ax1, ax2,ax3, ax4) = plt.subplots(4, 1, figsize=(10,40))
+fig, (ax1, ax2,ax3, ax4) = plt.subplots(4, 1, figsize=(10,32))
                                             # gridspec_kw={'hspace': 0.4, 'wspace': 0.4})
 
 x = np.array([2.000, 0.589, 0.053, 0.063])
