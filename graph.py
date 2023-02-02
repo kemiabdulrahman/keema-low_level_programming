4679bc382f1095b2088d36446fb29fe92da89645
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-02-02 07:11:37 +0100

    task done

--- graph.py
+++ graph.py
@@ -1,33 +1,23 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.array([2.000, 0.589, 0.053, 0.063]) 
-y = np.array([0.476, 0.391, 0.042, 0.007]) 
+x = np.array([2.000, 0.589, 0.053, 0.063])
+y = np.array([0.476, 0.391, 0.042, 0.007])
 
-plt.title("Graph Of Screen Analysis Experiment Using Set of Sieve A") 
-plt.xlabel("Particle Size") 
-plt.ylabel("Mass Fraction") 
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 
-plt.subplot(2, 1, 1, gridspec_kw={'height_ratios': [3, 1]}) 
-plt.plot(x, y) 
-plt.show 
+ax1.plot(x, y)
+ax1.set_title("Graph Of Screen Analysis Experiment Using Set of Sieve A")
+ax1.set_xlabel("Particle Size")
+ax1.set_ylabel("Mass Fraction")
 
-# Two lines to make our compiler able to draw: 
+x = np.array([2.000, 0.589, 0.053, 0.063])
+y = np.array([0.476, 0.867, 0.909, 0.916])
 
-####### 
+ax2.plot(x,y)
+ax2.set_xlabel("Particle Size")
+ax2.set_ylabel("Cumulative Fraction")
 
-# plot 2: 
-
-x = np.array([2.000, 0.589, 0.053, 0.063]) 
-y = np.array([0.476, 0.867, 0.909, 0.916]) 
-
-plt.xlabel("Particle Size") 
-plt.ylabel("Cumulative Fraction") 
-plt.subplot(2, 1, 2, gridspec_kw={'height_ratios': [1, 3]}) 
-plt.plot(x, y) 
-
-plt.grid() 
-plt.show() 
-
-plt.savefig('screen_analysis.pdf')
+plt.subplots_adjust(hspace=0.5)
+plt.show()
 
