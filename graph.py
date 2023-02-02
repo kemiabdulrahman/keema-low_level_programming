27147cf0f3d2dbb7e94a97328c7b797b06f3acfa
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-02-02 07:26:58 +0100

    task done

--- graph.py
+++ graph.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 x = np.array([2.000, 0.589, 0.053, 0.063])
 y = np.array([0.476, 0.391, 0.042, 0.007])
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
 
 ax1.plot(x, y)
 ax1.set_title("Graph Of Screen Analysis Experiment Using Set of Sieve A")
-ax1.set_xlabel("Particle Size")
 ax1.set_ylabel("Mass Fraction")
 
 x = np.array([2.000, 0.589, 0.053, 0.063])
@@ -18,6 +17,6 @@ ax2.plot(x,y)
 ax2.set_xlabel("Particle Size")
 ax2.set_ylabel("Cumulative Fraction")
 
-plt.subplots_adjust(hspace=0.5)
+fig.tight_layout()
 plt.show()
 
