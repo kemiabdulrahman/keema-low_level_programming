bdc59e00a618dc76ce82ecd1d79c6bc26beb06f8
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-02-02 11:52:26 +0100

    task done

--- graph1.py
+++ graph1.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 fig, (ax1, ax2,ax3, ax4) = plt.subplots(4, 1, figsize=(10,22))
                                             # gridspec_kw={'hspace': 0.4, 'wspace': 0.4})
 
-x = np.array([2.000, 0.589, 0.053, 0.063])
-y = np.array([0.476, 0.391, 0.042, 0.007])
+x = np.array([10,28,53,63,"PAN"])
+y = np.array([0.476, 0.391, 0.042, 0.007,0.073])
 
 ax1.plot(x, y)
 ax1.set_title("Graph Of Screen Analysis Experiment Using Set of Sieve A")
@@ -14,15 +14,15 @@ ax1.set_ylabel("Mass Fraction")
 ax1.grid()
 
 
-x = np.array([2.000, 0.589, 0.053, 0.063])
-y = np.array([0.476, 0.867, 0.909, 0.916])
-
+x = np.array([10,28,53,63,"PAN"])
+y = np.array([0.476,0.867,0.909,0.916,0.989])
 ax2.plot(x,y)
 ax2.set_xlabel("Particle Size")
 ax2.set_ylabel("Cumulative Fraction")
 ax2.grid()
-x = np.array([10,28,53,63,'PAN'])
-y = np.array([0.476,0.392,0.042,0.007,0.073])
+
+x = np.array([10,14,18,30,50,70,"PAN"])
+y = np.array([0.5650,0.1200,0.1210,0.0790,0.0390,0.0427,0.0143])
 
 ax3.plot(x, y)
 ax3.set_title("Graph Of Screen Analysis Experiment Using Set of Sieve B")
