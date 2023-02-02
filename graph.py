18dc938156ca769f0f920e96f2c25a87c20e74e8
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-02-02 05:48:39 +0100

    task done

--- graph.py
+++ graph.py
@@ -11,15 +11,14 @@ x = np.array([2.000, 0.589, 0.053, 0.063])
 y = np.array([0.476, 0.391, 0.042, 0.007])
 
 plt.title("Graph Of Screen Analysis Experiment Using Set of Sieve A")
-plt.xlabel("mass fraction")
-plt.ylabel("particle size")
+plt.xlabel("Particle Size")
+plt.ylabel("Mass Fraction")
 
 
 
 plt.subplot(2, 1, 1)
 plt.plot(x, y)
-
-plt.grid()
+plt.show
 
 
 #Two  lines to make our compiler able to draw:
@@ -31,10 +30,13 @@ plt.grid()
 x = np.array([2.000, 0.589, 0.053, 0.063])
 y = np.array([0.476, 0.867, 0.909, 0.916])
 
-plt.xlabel("cumulative fraction")
-plt.ylabel("particle size")
+plt.xlabel("Particle Size")
+plt.ylabel("Cumulative Fraction")
 plt.subplot(2, 1, 2)
 plt.plot(x,y)
+
+
+
 plt.grid()
 plt.show()
 
